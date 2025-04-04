ac231ca77327393fcdd8758de0b494071efc1999
Valentin Senessi <valensenefacultad@gmail.com>
2025-04-04 11:39:48 -0300

    Mejora de la impresión de ronda y calculando MVP por ronda

--- src/funciones.py
+++ src/funciones.py
@@ -4,4 +4,15 @@ def imprimirRonda(lista,ronda):
         print (" ")
         for event,value in stats.items():
             print(f" {event} : {value} ", end= " ")
-        print (" ")
+        kills = stats["kills"]
+        assists = stats["assists"]
+        deaths = stats["deaths"]
+        puntaje = kills * 3 + assists * 1 - (1 if deaths else 0)
+        lista[ronda][jugador]["puntos"] = puntaje
+        print("puntos : ", puntaje)
+        print(" ")
+    jugadores_ordenados = sorted(lista[ronda].items(), key=lambda x: x[1]["puntos"], reverse=True)
+    mvp = jugadores_ordenados[0][0]
+    print("El MVP de la ronda es: ",mvp)
+    
+    
